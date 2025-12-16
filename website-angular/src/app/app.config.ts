33cf9e8f26e903f9290d5e70e40759d63065d985
gowthamkoffcl <gowthamkoffcl@gmail.com>
2025-12-16 07:33:03 +0530

    added the netlify conf

--- website-angular/src/app/app.config.ts
+++ website-angular/src/app/app.config.ts
@@ -1,14 +1,9 @@
+// src/app/app.config.ts (CLIENT-SIDE)
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideClientHydration(),
-    provideHttpClient(withFetch())
-  ]
+  providers: [provideRouter(routes), provideHttpClient(withFetch())],
 };
--- website-angular/src/app/core/core.module.ts
+++ website-angular/src/app/core/core.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero';
 import { ServicesComponent } from './components/services/services';
 import { Products } from './components/products/products';
 import { Staff } from './components/staff/staff';
-import { Appointments } from './components/appointments/appointments';
-import { FormsModule } from '@angular/forms';
 import { CoreRoutingModule } from './core-routing-module';
+import { Appointments } from './components/appointments/appointments';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { CoreRoutingModule } from './core-routing-module';
     Staff,
     Appointments,
   ],
-  imports: [CommonModule, RouterModule, FormsModule, CoreRoutingModule],
+  imports: [CoreRoutingModule,CommonModule, RouterModule, FormsModule, ],
   exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
